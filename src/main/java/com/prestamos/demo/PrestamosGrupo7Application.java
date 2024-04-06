6c64c74866b3a47a21f4d95a31fd96f7a384ae82
loana14 <loanapumayali14@gmail.com>
2024-04-05 22:26:01 -0500

    first commit

---
+++ src/main/java/com/prestamos/demo/PrestamosGrupo7Application.java
@@ -0,0 +1,13 @@
+package com.prestamos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosGrupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/prestamos/demo/PrestamosGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestamos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
