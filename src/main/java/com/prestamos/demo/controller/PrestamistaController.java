7338a6f2ed41dc1b4d14a32abb80723e200dd02d
Billy <i202122729@cibertec.edu.pe>
2024-04-18 23:02:57 -0500

    FORMULARIO Y METODO DE ACTUALIZAR

--- src/main/java/com/prestamos/demo/controller/PrestamistaController.java
+++ src/main/java/com/prestamos/demo/controller/PrestamistaController.java
@@ -103,6 +103,67 @@ public class PrestamistaController {
 	     }
 	 }
 	 
+	 
+	 
+	 
+	 // editar prestamistas 
+	 
+	 
+	 @GetMapping("/editarPrestamista/{id}")
+	 public String mostrarFormEditar(@PathVariable("id") int id, Model model) {
+		 Usuarios usuario = ususerv.obtenerId(id);
+		    if (usuario != null) {
+		        model.addAttribute("usuarioP", usuario);
+		        model.addAttribute("distritos", disService.listaDistritos());
+		        return "editarPrestamista";
+		    } else {
+		        // Manejar el caso en el que no se encuentre el usuario
+		        // Por ejemplo, puedes redirigir a una página de error o mostrar un mensaje de error
+		        return "error";
+		    }
+	 }
+	 
+	 @PostMapping("/editarPrestamista/{id}")
+	 public String actualizarUsuario(@PathVariable("id") int id, @ModelAttribute("usuarioP") Usuarios usuario, Model model) {
+		    Usuarios usuarioExistente = ususerv.obtenerId(id); // Obtener el usuario existente de la base de datos
+		    if (usuarioExistente != null) {
+		        // Actualizar los campos del usuario existente con los valores del formulario
+		        usuarioExistente.setNombre(usuario.getNombre());
+		        usuarioExistente.setApellido(usuario.getApellido());
+		        usuarioExistente.setCorreo(usuario.getCorreo());
+		        usuarioExistente.setDni(usuario.getDni());
+		        usuarioExistente.setTelefono(usuario.getTelefono());
+		        usuarioExistente.setContrasenia(usuario.getContrasenia());
+		        usuarioExistente.setNacimiento(usuario.getNacimiento());
+		        // Actualizar otras propiedades si es necesario (por ejemplo, idUsuario y distrito)
+
+		        // Guardar el usuario actualizado en la base de datos
+		        ususerv.updateUsuario(usuarioExistente);
+
+		        // Redirigir a la página de éxito
+		        return "redirect:/Prestamista/editarPrestamista/" + id + "?success";
+		    } else {
+		        // Manejar el caso en el que no se encuentre el usuario
+		        // Por ejemplo, puedes redirigir a una página de error o mostrar un mensaje de error
+		        return "error";
+		    }
+		}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 	 @GetMapping("/eliminarPrestamista/{id}")
 	 public String eliminarJefePrestamista(@PathVariable("id") int id, RedirectAttributes flash) {
 		 
